33c8d85df83b075f16987996adb81a41b6662fb2
Alex <agonzalezg94@gmail.com>
2018-03-24 19:51:00 +0100

    Definicion de la clase Person

---
+++ lib/Person.h
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/**!    \class Person
+ *          \brief  Describe la clase person. Define a una persona
+ */
+using namespace std;
+
+class Person
+{
+    private:
+        int iEdad;
+        std::string sNombre[10];
+    public:
+        
+        //!< Constructor por defecto
+        Person();
+        //!< Constructor con edad y nombre
+        Person(int iaEdad, std::string saNombre);
+
+        //!< Destructor
+        ~Person();
+
+        std::string getNombre();
+        int getEdad();
+        void setNombre(std::string argSNombre);
+        void setEdad(int argiEdad);
+}
---
+++ src/Person.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "lib/Person.h"
+
+Person::Person()
+{
+    iEdad = 20;
+    sNombre = "Pepito";  
+
+}
+
+Person::Person(int iaEdad, std::string saNombre)
+{
+    iEdad = iaEdad;
+    sNombre = saNombre;
+
+}
+
+Person::~Person()
+{
+
+}
+
+int Person::getEdad()
+{
+    return this->iEdad;
+}
+
+std::string Person::getNombre()
+{
+    return this->sNombre;
+}
+
+void Person::setEdad(int argiEdad)
+{
+    this->iEdad = argiEdad;
+}
+
+void Person::setNombre(std::string argSNombre)
+{
+    this->sNombre = argSNombre;
+}
